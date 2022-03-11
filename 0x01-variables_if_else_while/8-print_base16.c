f2e96dbba7e0ba804b69b1be0b9bbf3d8b59baf2
anonimo-junino <fediamedenou@gmail.com>
2022-03-11 20:17:15 +0000

    Process 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print digit in base 16 using only putchar and lowercase
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		putchar(i);
+	}
+	for (i = 'a' ; i <= 'f' ; i++)
+	{
+		putchar(i)
+	}
+	putchar('\n');
+	return (0);
+}
