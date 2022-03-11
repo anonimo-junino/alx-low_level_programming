cb35ebacdf869121e3dccbe04bdc0ecb74906a5c
anonimo-junino <fediamedenou@gmail.com>
2022-03-11 11:55:10 +0000

    Process 1. Compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,8 +4,8 @@
  *
  * Return: 0 after printing the function
  */
-int void(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
