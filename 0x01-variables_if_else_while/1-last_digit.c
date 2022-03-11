9b67d0fa6b72084be4d2e673f72b73c1aae13802
anonimo-junino <fediamedenou@gmail.com>
2022-03-11 17:21:18 +0000

    Process 1. The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,9 +16,9 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 5)
 	printf("%d and is greater than 5\n", n);
-	if (n = 0)
+	else if (n == 0)
 	printf("%d and is 0\n", n);
-	if ((n < 6) && (n != 0))
+	else
 	printf("%d and is less than 6 and not 0\n", n);
 	return (0);
 }
