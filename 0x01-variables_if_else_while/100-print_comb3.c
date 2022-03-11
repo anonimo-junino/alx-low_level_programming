13567ee31bb7af4143dc0d20c8c53f511270e899
anonimo-junino <fediamedenou@gmail.com>
2022-03-11 20:49:48 +0000

    Process 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible conbinations of two-digit numbers
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		for (i = 49; j <= 57; j++)
+		{
+			if (i != j)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
