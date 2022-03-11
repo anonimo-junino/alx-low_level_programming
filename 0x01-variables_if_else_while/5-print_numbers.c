fe15c45a2328bc3d8224219f6922679068883e3b
anonimo-junino <fediamedenou@gmail.com>
2022-03-11 19:43:02 +0000

    Process 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 10 ; i >=0 ; i--)
+	{
+		printf("%d \n", i);
+	}
+	return (0);
+}
