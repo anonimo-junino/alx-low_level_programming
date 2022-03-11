dabac602d589f018dabc33be670d0708e63f233e
anonimo-junino <fediamedenou@gmail.com>
2022-03-11 18:34:15 +0000

    Process 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lowercase
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'Z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
