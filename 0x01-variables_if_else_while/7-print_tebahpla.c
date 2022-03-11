9bbcd1e52c60c69e5dac49f25b503bab2b6de17a
anonimo-junino <fediamedenou@gmail.com>
2022-03-11 20:07:12 +0000

    Process 6. Numberz

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabet in lowercase reverse
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a' ; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
