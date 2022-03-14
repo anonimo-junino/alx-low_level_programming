e272978da3a239f56a25df147675e01fdf159a7b
anonimo-junino <fediamedenou@gmail.com>
2022-03-14 18:32:08 +0000

    Process task 8 UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,7 @@
  */
 int main(void)
 {
-	fprint(stderr, "and that piece of art is useful \" - Dora Korpar, 2015-10-19");
+	fprint(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19");
+
 	return (1);
 }
