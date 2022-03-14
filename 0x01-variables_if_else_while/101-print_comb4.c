aa157a07e061ecf7563400af701be98b693375f6
anonimo-junino <fediamedenou@gmail.com>
2022-03-14 19:13:42 +0000

    Process task 11. The success combination in business is

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible conbinations of two-digit numbers
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48 ; i <= 56 ; i++)
+	{
+		for (j = 49 ; j <= 57 ; j++)
+		{
+			for (k = 50; k <=58; k++)
+			{
+				if ((k > J) && (j > i))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+
+					if (i != 56 || j != 57 || k != 58)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
