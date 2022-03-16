2231ffd21913013f7524b1415c0e649aacfa2611
anonimo-junino <fediamedenou@gmail.com>
2022-03-16 08:01:44 +0000

    Process 0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints _putchar follewed by a new line
+ *
+ * Return : (0) Success
+ */
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return 0;
+}
