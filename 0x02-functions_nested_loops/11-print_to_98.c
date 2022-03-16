0394720a72262bcc177241af178bdc7d7a8f7f2a
anonimo-junino <fediamedenou@gmail.com>
2022-03-16 15:31:51 +0000

    Process 11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print natural numbers
+ * @n: Top limit number
+ *
+ * Description: This is function that prints all naural numbers
+ * from n to 98 followed by a new line
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		printf("%d",i);
+		if (i < 98)
+			printf(", ");
+	}
+}
