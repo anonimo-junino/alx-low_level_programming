30f1c0f1b685b34c9792739fe0f00a4fc5fcb637
anonimo-junino <fediamedenou@gmail.com>
2022-03-16 15:12:54 +0000

    Process 10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Adding number
+ * @a: first number to add
+ * @b: second number to add
+ *
+ * Description: This program adds two integers and returns the result
+ * Return: (sum) sum
+ */
+int add(int a, int b)
+{
+	int sum;
+	
+	return (a+b);
+}
