293613dc3f50c3634865d1e1e9ff14cd20c6f6ed
anonimo-junino <fediamedenou@gmail.com>
2022-03-16 12:26:19 +0000

    Process 7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: number to be checked
+ *
+ * Return: (rval) Success ortherwise nothing
+ */
+int print_last_digit(int n)
+{
+	return (n %d);
+}
