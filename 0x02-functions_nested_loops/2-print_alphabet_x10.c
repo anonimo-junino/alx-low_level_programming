c810bd146d2b24242d9c47c9955b0a3d6f29b5cb
anonimo-junino <fediamedenou@gmail.com>
2022-03-16 09:25:55 +0000

    Process 2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,8 @@
 
 /**
  * print_alphabet_x10 - print alphabet
- *
- * Description: Prints 10 times the alphabet in lowercase 
+ * 
+ * Description: Prints 10 times the alphabet in lowercase
  * followed by a new line
  */
 void print_alphabet_x10(void)
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 	int i;
 	char j;
 
-	for (i = 0; i <=9 ; i++)
+	for (i = 0; i <= 9 ; i++)
 	{
 		for (j = 'a'; j <= 'z'; j++)
 		{
