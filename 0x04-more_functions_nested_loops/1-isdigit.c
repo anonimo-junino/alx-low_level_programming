2830dd2c9be3e39046d0e37b48aaf1e1cfffefe0
anonimo-junino <fediamedenou@gmail.com>
2022-03-17 11:58:30 +0000

    Process 1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -5,16 +5,19 @@
  * @c: value to be checked
  *
  * Description: Function that checks for a digit
- * Returns: On Success 1, otherwise 0
+ * Return: On Success 1, otherwise 0
  */
 int _isdigit(int c)
 {
 	int rval;
+	int i;
 
-	if (c >= 48 && c <= 57)
-		rval = 1;
-	else
-		rval = 0;
-
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == c)
+			rval = 1;
+		else
+			rval = 0;
+	}
 	return (rval);
 }
