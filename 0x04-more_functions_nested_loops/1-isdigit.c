76424e2335b6b17544eba85c21b99e9117cade84
anonimo-junino <fediamedenou@gmail.com>
2022-03-17 15:45:51 +0000

    Process 1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -13,30 +13,30 @@ int _isdigit(int c)
 	int rval;
 	int i;
 
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
-		rval = 0;
-	else if (c >= 48 && c <= 57)
-	{
-		for (i = 0; i <= 9; i++)
-		{
-			if (i == c)
-				rval = 1;
-			else
-				rval = 0;
-		}
-	}
-	else
-		rval = 0;
-	
 	/*
-	*for (i = 0; i <= 9; i++)
+	*if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	*	rval = 0;
+	*else if (c >= 48 && c <= 57)
 	*{
-	*	if (i == c)
-	*		rval = 1;
-	*	else
-	*		rval = 0;
+	*	for (i = 0; i <= 9; i++)
+	*	{
+	*		if (i == c)
+	*			rval = 1;
+	*		else
+	*			rval = 0;
+	*	}
 	*}
+	*else
+	*	rval = 0;
 	*/
-
+	
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == c)
+			rval = 1;
+		else
+			rval = 0;
+	}
+	
 	return (rval);
 }
