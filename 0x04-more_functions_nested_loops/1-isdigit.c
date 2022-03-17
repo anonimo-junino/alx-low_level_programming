a162e1174afa965a2caeae2b836d80b8de1338e1
anonimo-junino <fediamedenou@gmail.com>
2022-03-17 12:32:58 +0000

    Process 1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,14 +10,38 @@
 int _isdigit(int c)
 {
 	int rval;
-	int i;
 
-	for (i = 0; i <= 9; i++)
-	{
-		if (i == c)
-			rval = 1;
-		else
-			rval = 0;
-	}
+	if (c == 0)
+		rval = 1;
+	else if (c == 1)
+		rval = 1;
+	else if (c == 2)
+		rval = 1;
+	else if (c == 3)
+		rval = 1;
+	else if (c == 4)
+		rval = 1;
+	else if (c == 5)
+		rval = 1;
+	else if (c == 6)
+		rval = 1;
+	else if (c == 7)
+		rval = 1;
+	else if (c == 8)
+		rval = 1;
+	else if (c == 9)
+		rval = 1;
+	else
+		rval = 0;
+
+	/*
+	 * for (i = 0; i <= 9; i++)
+	*  {
+	*	if (i == c)
+	*		rval = 1;
+	*	else
+	*		rval = 0;
+	*}
+	*/
 	return (rval);
 }
--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdbool.h>
 
 /**
  * main - check the code
@@ -9,11 +10,15 @@
 int main(void)
 {
 	char c;
+	bool x;
 
 	c = '0';
 	printf("%c: %d\n", c, _isdigit(c));
 	c = 'a';
 	printf("%c: %d\n", c, _isdigit(c));
+	printf("------------------------\n");
+	x = '0' == 0;
+	printf(" '0' = 0 : %d\n", x);
 
 	return (0);
 }
