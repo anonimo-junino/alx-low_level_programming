2b0c4723ef031f95b181b4617324f6b613262b0b
anonimo-junino <fediamedenou@gmail.com>
2022-03-17 16:02:21 +0000

    Process 2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+/**
+ * mul - multiplies
+ * @a: first number to multiplier
+ * @b: second number to multiplier
+ *
+ * Description: Function that multiplies two integers
+ * Return: 'mul' on Success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
