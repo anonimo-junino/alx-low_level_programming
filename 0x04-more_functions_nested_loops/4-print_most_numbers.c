a2ed86d4559839b6f1394ced7939e5f35834036d
anonimo-junino <fediamedenou@gmail.com>
2022-03-17 16:28:01 +0000

    Process 4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,14 @@
  * followed by a new line
  * Return: return void
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 48; i <= 57; i++)
 	{
 		if (i != 50 && i != 52)
-		_putchar(i);
+			_putchar(i);
 	}
 	_putchar('\n');
 }
