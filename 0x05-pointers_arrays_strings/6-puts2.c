268e4675fddc07970795716ff1b8c934452142ea
anonimo-junino <fediamedenou@gmail.com>
2022-03-22 16:35:34 +0000

    Process 6. void puts2(char *str)

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - puts other characters
+ * @str: string to be checked
+ *
+ * Description: Function that prints every other character of a string starting
+ * with the first character followed by a new line
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int length = 0, i = 0;
+
+	do {
+		length++;
+	} while (str[length] != '\0');
+
+	for (i = 0; i < length; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
