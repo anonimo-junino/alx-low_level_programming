61df0825963cf71105077f434e3aa7a05b7790cc
anonimo-junino <fediamedenou@gmail.com>
2022-03-22 08:21:01 +0000

    Process void reset_to_98(int *n)

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a number to 98
+ * @n : The number to be reset
+ *
+ * Description: Reset a number to 98
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98 ;
+}
