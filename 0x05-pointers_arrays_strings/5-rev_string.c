ea2bcb0ce6cc579c40f381a59c404ee6b0308018
anonimo-junino <fediamedenou@gmail.com>
2022-03-22 16:11:37 +0000

    Process 5. void rev_string(char *s)

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,7 +11,7 @@
 void rev_string(char *s)
 {
 	int nbrchar = 0;
-	int i = 0, j =0;
+	int i = 0, j = 0;
 	char v1, v2;
 
 	if (*s != 0)
@@ -21,7 +21,7 @@ void rev_string(char *s)
 		} while (s[nbrchar] != '\0');
 
 		j = nbrchar - 1;
-		
+
 		while (j > i)
 		{
 			v1 = s[i];
