f846624e6f49b4708d190113da319de6c1fca590
anonimo-junino <fediamedenou@gmail.com>
2022-03-22 13:22:44 +0000

    Process 3. void _puts(char *str)

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints string
+ * @str: string to output
+ *
+ * Description: Function that prints a string followed by a new line to stdout
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	if (*str != 0)
+	{
+		do {
+			_putchar(str[i]);
+			i++;
+		} while (str[i] != '\0');
+	}
+}
