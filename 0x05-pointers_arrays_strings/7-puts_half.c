6a659a3a7d87eac02dc58364944f56e7fd843d4e
anonimo-junino <fediamedenou@gmail.com>
2022-03-22 17:07:53 +0000

    Process 7. void puts_half(char *str)

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - prints string half
+ * @str: string to be checked
+ *
+ * Description: Function that prints half of a string followed by a new line
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int length = 0, i = 0;
+
+	do {
+		length++;
+	} while (str[length] != '\0');
+	
+
+	while (str[i] != '\0')
+	{
+		if (i > (length -1) / 2)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
