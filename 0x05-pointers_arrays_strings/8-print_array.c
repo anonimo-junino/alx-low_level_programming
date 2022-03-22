ca226f855dcd05ce5efe963e71eb593ef93e6711
anonimo-junino <fediamedenou@gmail.com>
2022-03-22 17:35:25 +0000

    Process 8. void print_array(int *a, int n)

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_array - array print
+ * @a: string array
+ * @n: array number of elements to be printed
+ *
+ * Description: Function that prints n elements of an array of integers
+ * followed by a new line
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i <= n; i++)
+		printf("%d, ", a[i]);
+}
