d9a85dac33f9cbc70eda73db6c470c193f31bfe5
anonimo-junino <fediamedenou@gmail.com>
2022-03-22 09:21:25 +0000

    Process 2. int _strlen(char *s)

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -9,10 +9,18 @@
 int main(void)
 {
 	int n;
+	int * p = &n;
 
 	n = 402;
 	printf("n=%d\n", n);
 	reset_to_98(&n);
 	printf("n=%d\n", n);
+	printf("size of n %li\n", sizeof(n));
+	printf("value of n %dn", n);
+
+	printf("size of p %li\n", sizeof(p));
+	/*printf("value of p %p \n", p);*/
+	printf("size of *p %li \n", sizeof(*p));
+	printf("value of *p %d \n", *p);
 	return (0);
 }
---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - the length of a string
+ * @s: string that length must be none
+ *
+ * Description: Function that returns the length of a string
+ * Return: on sucess length
+ */
+int _strlen(char *s)
+{
+	int nbrChar = 0;
+
+	do {
+		nbrChar++;
+	} while (s[nbrChar] != '\0');
+
+	return (nbrChar);
+}
