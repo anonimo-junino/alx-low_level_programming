64769244f3496d8dd8ab6011349a16f7bc8194c9
anonimo-junino <fediamedenou@gmail.com>
2022-03-24 17:47:59 +0000

    Process for 6. char *cap_string(char *)

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes string
+ * @str: string to be capitalized
+ *
+ * Description: Function that capitalizes all words of a string
+ * Return: capitalize string
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+	int cptlz = 13;
+	int ct = 0;
+	char sprt[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (str[ct])
+	{
+		i = 0;
+
+		while (i < cptlz)
+		{
+			if ((ct == 0 || str[ct - 1] == sprt[]) && (str[ct] >= 97 && str[ct] <= 122))
+				str[ct] -= 32;
+
+			i++;
+		}
+		ct++;
+	}
+
+	return (str);
+}
