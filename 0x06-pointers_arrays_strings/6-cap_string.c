b4d4b8ba537076adce94e21aaac91a8cd7780979
anonimo-junino <fediamedenou@gmail.com>
2022-03-24 18:03:06 +0000

    Process for 6. char *cap_string(char *)

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *str)
 	int i = 0;
 	int cptlz = 13;
 	int ct = 0;
-	char sprt[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+	char spr[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
 
 	while (str[ct])
 	{
@@ -21,7 +21,7 @@ char *cap_string(char *str)
 
 		while (i < cptlz)
 		{
-			if ((ct == 0 || str[ct - 1] == sprt[]) && (str[ct] >= 97 && str[ct] <= 122))
+			if ((ct == 0 || str[ct - 1] == spr[i]) &&(str[ct] >= 97 && str[ct] <= 122))
 				str[ct] -= 32;
 
 			i++;
