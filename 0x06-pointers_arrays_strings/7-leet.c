92d6000a3b64a5fd500a33654353de5058ceb2fb
anonimo-junino <fediamedenou@gmail.com>
2022-03-29 16:06:41 +0000

    This is first commit for 0x07-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - encodes
+ * @str: string to encodes
+ *
+ * Description: Function that encodes a string into 1337
+ * Return: str encoded
+ */
+char *leet(char *str)
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is README.md for 0x06-pointers_arrays_strings
+This is README.md for 0x07-pointers_arrays_strings
